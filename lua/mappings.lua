86918fd462390eb82ca2dd51f788f8885e64b95e
Siripuram Dileep Kumar <dileep.kumar@mongodb.com>
2026-06-28 03:56:40 +0530

    floating terminal

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -3,7 +3,8 @@ require "nvchad.mappings"
 local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
-map("i", "jk", "<ESC>")
+map("i", "kl", "<ESC>")
+map("i", "kl", "<ESC>")
 
 -- File explorer
 map("n", "<leader>e", "<cmd>NvimTreeToggle<cr>", { desc = "Toggle file tree (left)" })
@@ -13,6 +14,22 @@ map("n", "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>", { desc = "Toggle T
 
 -- Format buffer
 map("n", "<leader>fm", function() require("conform").format() end, { desc = "Format buffer" })
+
+-- Terminal
+map("t", "kl", "<C-\\><C-n>", { desc = "Exit terminal mode" })
+map("t", "lk", "<C-\\><C-n>", { desc = "Exit terminal mode" })
+map("n", "<leader>t", function()
+  require("nvchad.term").toggle {
+    pos = "float",
+    id = "floatTerm",
+    float_opts = {
+      row = 0.05,
+      col = 0.05,
+      width = 0.9,
+      height = 0.85,
+    },
+  }
+end, { desc = "Toggle floating terminal" })
 
 -- LSP navigation
 map("n", "gd", vim.lsp.buf.definition, { desc = "Go to definition" })
