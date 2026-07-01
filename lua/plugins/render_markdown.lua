d28e688fa404f984386326796e0f678607391dc2
S.Dileep Kumar <dummy email>
2026-07-01 13:31:29 +0530

    markdown anti conceal change

--- lua/plugins/render_markdown.lua
+++ lua/plugins/render_markdown.lua
@@ -2,5 +2,7 @@ return {
   "MeanderingProgrammer/render-markdown.nvim",
   dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
   ft = { "markdown" },
-  opts = {},
+  opts = {
+    anti_conceal = { enabled = false },
+  },
 }
